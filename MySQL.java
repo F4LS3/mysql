d5f88b674c7a17fd61da5ccd66c0c78d80198704
F4LS3 <37489275+F4LS3@users.noreply.github.com>
2018-04-03 12:02:40 +0200

    Update MySQL.java

--- MySQL.java
+++ MySQL.java
@@ -1,78 +1,53 @@
 package de.f4ls3.utils;
 
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.io.File;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySQL {
 
-    private static File file = new File("plugins/TheWalkingDead/MySQL", "mysql.yml");
-    private static YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
-
-    public MySQL() {
-        try {
-            cfg.options().copyDefaults(true);
-            cfg.addDefault("username", "username");
-            cfg.addDefault("password", "password");
-            cfg.addDefault("host", "host");
-            cfg.addDefault("port", "3306");
-            cfg.addDefault("database", "database");
-            cfg.save(file);
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static Connection con;
-    private static boolean isConnected;
-    private static String user = (String) cfg.get("username");
-    private static String pw = (String) cfg.get("password");
-    private static String host = (String) cfg.get("host");
-    private static String port = (String) cfg.get("port");
-    private static String database = (String) cfg.get("database");
-
+    public static String username;
+    public static String password;
+    public static String database;
+    public static String host;
+    public static String port;
+    public static Connection con;
 
     public void connect() {
-        try {
-            if (con != null && !con.isClosed()) {
-                return;
+        if(!isConnected()) {
+            try {
+                con = DriverManager.getConnection("jdbc:mysql://"+ host + ":" + port + "/" + database, username, password);
+                Bukkit.getConsoleSender().sendMessage(Var.getPrefix() + "§2MySQL successfully connected!");
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://" + host+":"+port+"/"+database, user, pw);
-            Bukkit.getConsoleSender().sendMessage(Var.getPrefix() + "§2MySQL successfully Connected!");
-        } catch(Exception e) {
-            e.printStackTrace();
         }
     }
 
     public void disconnect() {
-        try {
-            if(!con.isClosed()) {
+        if(isConnected()) {
+            try {
                 con.close();
-                Bukkit.getConsoleSender().sendMessage(Var.getPrefix() + "§2MySQL Connection is successfully Closed!");
-            } else {
-                Bukkit.getConsoleSender().sendMessage(Var.getPrefix() + "§cCan't close Connection: error: CONNECTION_ALREADY_CLOSED");
+                Bukkit.getConsoleSender().sendMessage(Var.getPrefix() + "§cMySQL successfully disconnected!");
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-        } catch(Exception e) {
-            e.printStackTrace();
         }
     }
 
-    public boolean connected() {
-        try {
-            if(con.isClosed()) {
-                isConnected = false;
-            } else if(!con.isClosed()) {
-                isConnected = true;
+    public static boolean isConnected() {
+        return con != null;
+    }
+
+    public static void createTable() {
+        if(isConnected()) {
+            try {
+                con.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS BannedPlayers (Spielername VARCHAR(100), UUID VARCHAR(100), Ende VARCHAR(100), Grund VARCHAR(1000))");
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-        } catch(SQLException e) {
-            e.printStackTrace();
         }
-        return isConnected;
     }
 }
